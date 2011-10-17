a5e6e3cdfc790d48988774edf8fef9ccdd48e7af
Rodney Degracia <rdegraci@gmail.com>
2011-10-16 20:21:36 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Famijobs::Application.config.secret_token = 'ccdb34965b9f593b8d6b1d77a48c1b7b29edce53ccc1b03cab3527ec28044350d30a6bb07a0f833556edf2ceba9feac3cfe37d64ce28dda430a509ce87dd8060'
