e7e924ce68532a76e644651aaf94d677b91382b5
Rodney Degracia <rdegraci@gmail.com>
2011-11-07 19:23:54 -0500

    Add Profile and job seekers to home page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,7 @@ class DashboardController < ApplicationController
   
   def index
       @positions = Position.find_all_by_open(true) || []
+      @jobseekers = Profile.find_all_by_available(true) || []
   end
   
 end
