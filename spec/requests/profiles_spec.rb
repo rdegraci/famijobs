59205ce77acb363bb42921a6b51ccbd9a73a8653
Rodney Degracia <rdegraci@DigDug.local>
2011-12-04 17:39:18 -0500

    Refactor Profile spec
    We will need to move the spec into an admin controller, later.

--- spec/requests/profiles_spec.rb
+++ spec/requests/profiles_spec.rb
@@ -1,10 +1,15 @@
 require 'spec_helper'
 describe "Profiles" do
-  describe "GET /profiles" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get profiles_path
-      response.status.should be(302)
-    end
-  end
+  
+  #
+  # TODO: Move this into an admin controller spec
+  #
+  # describe "GET /profiles" do
+  #   it "works! (now write some real specs)" do
+  #     # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+  #     get profiles_path
+  #     response.status.should be(200)
+  #   end
+  # end
+  
 end
