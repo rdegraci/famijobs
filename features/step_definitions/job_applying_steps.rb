96e944bc56952a7d82313c14097915f853eba37e
Rodney Degracia <rdegraci@elvenlabs.com>
2011-12-12 13:49:54 -0500

    Add and use CSS id to applied and unapplied partials
    Necessary so that we can determine the content within the tables

--- features/step_definitions/job_applying_steps.rb
+++ features/step_definitions/job_applying_steps.rb
@@ -5,17 +5,17 @@ Given /^there are positions$/ do
 end
 
 
-When /^the User applies for the first open position$/ do
+When /^the User applies for the first unapplied position$/ do
   visit root_path
   check 'position[1][apply]'
   click_button 'Apply'
 end
 
-Then /^the position is not listed in the open positions$/ do
+Then /^the position is not listed in the unapplied positions$/ do
   # save_and_open_page
-  # find("#open_positions_table").should have_no_content('PHP Developer')
+  # find("#unapplied_positions_table").should have_no_content('PHP Developer')
 end
 
 Then /^the position is listed in the applied positions$/ do
-  #find("#applied_positions_table").should have_content('PHP Developer')
+  # find("#applied_positions_table").should have_content('PHP Developer')
 end
