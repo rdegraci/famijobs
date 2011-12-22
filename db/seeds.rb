5d99241115a274df6585cf0c8097e2a61ecd7b65
Rodney Degracia <rdegraci@elvenlabs.com>
2011-12-22 17:01:14 -0500

    Fix the seeds.rb
    Add missing virtual attributes to User

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,8 +26,8 @@ Profile.create([
 
 
 User.create([
-  { :encrypted_password => "$2a$10$AvaG69ZHdb3xruEZKs36NuS.DVADuhW2z53BfSVEjj5Yfl2FJTMwS", :created_at => "2011-12-03 19:07:46", :last_sign_in_ip => "127.0.0.1", :updated_at => "2011-12-03 19:56:31", :last_sign_in_at => "2011-12-03 19:09:40", :sign_in_count => 3, :reset_password_token => nil, :current_sign_in_ip => "127.0.0.1", :reset_password_sent_at => nil, :remember_created_at => nil, :current_sign_in_at => "2011-12-03 19:56:31", :email => "user1@some.com" },
-  { :encrypted_password => "$2a$10$qaqHJK3O8fQoOJnmr.cxwOvoBtb9lFfBPYI9PHqTlbqrF5EQwFt9K", :created_at => "2011-12-03 19:08:06", :last_sign_in_ip => "127.0.0.1", :updated_at => "2011-12-03 19:58:45", :last_sign_in_at => "2011-12-03 19:08:06", :sign_in_count => 2, :reset_password_token => nil, :current_sign_in_ip => "127.0.0.1", :reset_password_sent_at => nil, :remember_created_at => nil, :current_sign_in_at => "2011-12-03 19:58:45", :email => "user2@some.com" }
+  { :password => '123456', :password_confirmation => '123456', :encrypted_password => "$2a$10$AvaG69ZHdb3xruEZKs36NuS.DVADuhW2z53BfSVEjj5Yfl2FJTMwS", :created_at => "2011-12-03 19:07:46", :last_sign_in_ip => "127.0.0.1", :updated_at => "2011-12-03 19:56:31", :last_sign_in_at => "2011-12-03 19:09:40", :sign_in_count => 3, :reset_password_token => nil, :current_sign_in_ip => "127.0.0.1", :reset_password_sent_at => nil, :remember_created_at => nil, :current_sign_in_at => "2011-12-03 19:56:31", :email => "user1@some.com" },
+  { :password => '123456', :password_confirmation => '123456', :encrypted_password => "$2a$10$qaqHJK3O8fQoOJnmr.cxwOvoBtb9lFfBPYI9PHqTlbqrF5EQwFt9K", :created_at => "2011-12-03 19:08:06", :last_sign_in_ip => "127.0.0.1", :updated_at => "2011-12-03 19:58:45", :last_sign_in_at => "2011-12-03 19:08:06", :sign_in_count => 2, :reset_password_token => nil, :current_sign_in_ip => "127.0.0.1", :reset_password_sent_at => nil, :remember_created_at => nil, :current_sign_in_at => "2011-12-03 19:58:45", :email => "user2@some.com" }
 ])
 
 
