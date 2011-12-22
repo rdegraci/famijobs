a9f8e6296a367183335d8e8f569bcca54870df4c
Rodney Degracia <rdegraci@elvenlabs.com>
2011-12-22 17:03:05 -0500

    Add devise support to rails-rspec
    Necessary, so that we can access current_user within the controllers
    
    Note: the current_user is  subject.current_user

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -1,4 +1,18 @@
-# Add Devise support to rspec
-RSpec.configure do |config|
-  config.include Devise::TestHelpers, :type => :controller
+
+module ControllerMacros
+  def login_admin
+    before(:each) do
+      @request.env["devise.mapping"] = Devise.mappings[:admin]
+      sign_in FactoryGirl.create(:admin) # Using factory girl as an example
+    end
+  end
+
+  def login_user
+    before(:each) do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      user = FactoryGirl.create(:user)
+      #user.confirm! # or set a confirmed_at inside the factory. Only necessary if you are using the confirmable module
+      sign_in user
+    end
+  end
 end
