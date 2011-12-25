c6ff73e1a37ee390bab2c4cc189f9bd66fae912e
Rodney Degracia <rdegraci@gmail.com>
2011-12-24 23:39:25 -0500

    Refactor login steps
    Take into account validations

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,6 +1,6 @@
 When /^a User has registered$/ do
   visit new_user_registration_path
-  fill_in 'Email', :with => 'rdegraci@gmail.com'
+  fill_in 'Email', :with => 'capybara@elvenlabs.com'
   fill_in 'Password', :with => '123456'
   fill_in 'Password confirmation', :with => '123456'
   click_button "Sign up"
@@ -8,20 +8,35 @@ When /^a User has registered$/ do
 end
 
 Then /^the dashboard will display open positions$/ do
-  user1 = FactoryGirl.create(:bob)
-  position = FactoryGirl.create(:rails_dev)
-  user1.positions << position
-  user1.save
-  
-  visit root_path
-  
   page.should have_content('Rails Developer')
 end
 
-Then /^the dashboard will display available job seekers$/ do
-  user = User.find_by_email('rdegraci@gmail.com')
-  user.profile = FactoryGirl.create(:profile)
-  user.profile.save!
+Then /^the dashboard will display available job seekers$/ do  
+  page.should have_content('Rodney Degracia')
+end
+
+
+When /^the User has created a profile$/ do
+  visit new_profile_path
+  fill_in 'Firstname', :with => 'Rodney'
+  fill_in 'Lastname', :with => 'Degracia'
+  fill_in 'Phone', :with => '123-555-1234'
+  click_button "Create Profile"
+  page.should have_content('Firstname: Rodney')
   visit root_path
-  page.should have_content('iOS Developer')
+end
+
+When /^there are positions$/ do  
+  FactoryGirl.create :php_dev
+  FactoryGirl.create :rails_dev
+  FactoryGirl.create :perl_dev
+end
+
+Given /^the user visits the Dashboard$/ do
+  visit root_path
+end
+
+Then /^the User will log out$/ do
+  visit root_path
+  click_link 'Sign out'
 end
